3a84d9fdb0079a0d06560f667385123953ed3369
zrx <616962687@qq.com>
2021-01-26 15:07:21 +0800

    test

--- crc32.h
+++ crc32.h
@@ -6,7 +6,7 @@
 extern void     Crc32_Reset(void);
 /* Return a 32-bit CRC of the contents of the buffer. */
 extern uint32_t Crc32_CalcBlock(uint32_t crc, const uint8_t *p, uint32_t len);
-void mian(eseaweq);
+
 #endif /*_CRC32_H_*/
 
  
